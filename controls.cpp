49fdfd6e5c423c33d7ca5c3b969ef43716f38675
OsvaldoComCH <eduhrsilva@gmail.com>
2024-10-08 11:40:20 -0300

    Created Control Classes

---
+++ controls.cpp
@@ -0,0 +1,71 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+class Button
+{
+    public:
+    HWND hwnd;
+
+    Button(){}
+
+    Button(HWND Parent, const wchar_t * Text, int XPos, int YPos, int Width, int Height, int Style)
+    {
+        HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(Parent, GWLP_HINSTANCE);
+        hwnd = CreateWindowEx
+        (
+            0, L"BUTTON", Text,
+            WS_CHILD | WS_VISIBLE | Style,
+            XPos, YPos, Width, Height,
+            Parent, NULL, hInstance, NULL
+        );
+    }
+
+    Button(HWND Parent, const wchar_t * Text, int XPos, int YPos, int Width, int Height)
+    {
+        return Button(Parent, Text, XPos, YPos, Width, Height, BS_DEFPUSHBUTTON);
+    }
+
+    int GetState()
+    {
+        return (int)SendMessage(hwnd, BM_GETSTATE, 0, 0);
+    }
+}
+
+class GroupBox : public Button
+{
+    public:
+    GroupBox(){}
+
+    GroupBox(HWND Parent, int XPos, int YPos, int Width, int Height)
+    : Button(Parent, NULL, XPos, YPos, Width, Height, BS_GROUPBOX){}
+}
+
+class RadioButton : public Button
+{
+    public:
+    RadioButton(){}
+
+    RadioButton(HWND Parent, const wchar_t * Text, int XPos, int YPos, int Width, int Height)
+    : Button(Parent, Text, XPos, YPos, Width, Height, BS_AUTORADIOBUTTON){}
+}
+
+class Edit
+{
+    public:
+    Edit(){}
+
+    Edit(HWND Parent, const wchar_t * Text, int XPos, int YPos, int Width, int Height)
+    {
+        HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(Parent, GWLP_HINSTANCE);
+        hwnd = CreateWindowEx
+        (
+            0, L"EDIT", NULL,
+            WS_CHILD | WS_VISIBLE | ES_NUMBER,
+            XPos, YPos, Width, Height,
+            Parent, NULL, hInstance, NULL
+        );
+    }
+}
